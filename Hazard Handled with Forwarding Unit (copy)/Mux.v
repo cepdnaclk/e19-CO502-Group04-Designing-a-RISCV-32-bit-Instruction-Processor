c7cd292073e1cbbf3ac8a2ca67f2ab3107a589ec
MahelaEkanayake <mahelaekanayake.aurelius@gmail.com>
2025-07-07 08:52:48 +0530

    modularized inter stage registers and initiated

--- Hazard Handled with Forwarding Unit (copy)/Mux.v
+++
@@ -1,7 +0,0 @@
-module Mux(select, A, B, Mux_out);
-    input select;
-    input [31:0] A, B;
-    output [31:0] Mux_out;
-
-    assign Mux_out = (select == 1'b1) ? A : B;
-endmodule
--- Hazard Handled with Forwarding Unit (copy)/ShiftLeft1.v
+++
@@ -1,8 +0,0 @@
-module ShiftLeft1 (
-    input  wire [31:0] in,
-    output wire [31:0] out
-);
-
-    assign out = in << 1;
-
-endmodule
